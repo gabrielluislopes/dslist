cccc6d38b38046114373640029e393a9ebaabd10
Gabriel Luis Lopes <ogabrielluislopes@gmail.com>
2024-11-13 18:33:16 -0300

    GET lists/{listId}/games

--- src/main/java/com/gabriellopes/dslist/services/GameService.java
+++ src/main/java/com/gabriellopes/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.gabriellopes.dslist.services;
 import com.gabriellopes.dslist.dto.GameDTO;
 import com.gabriellopes.dslist.dto.GameMinDTO;
 import com.gabriellopes.dslist.entities.Game;
+import com.gabriellopes.dslist.projections.GameMinProjection;
 import com.gabriellopes.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,13 @@ public class GameService {
     public GameDTO findById(Long id){
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
 }
